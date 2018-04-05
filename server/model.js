10ae845e44e1cbc7446e1b6d29c9e310ab0464a7
Lisa Jetton <ldjetton21@gmail.com>
2018-04-04 23:10:26 -0500

    framework foundation

---
+++ server/model.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var NewsSchema = new Schema({
+  title: {
+    type: String
+  },
+  date: {
+    type: Date
+  },
+  url: {
+    type: String
+  }
+});
+
+var News = mongoose.model("News", NewsSchema);
+module.exports = News;
